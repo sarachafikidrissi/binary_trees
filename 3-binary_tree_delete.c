23579c49b96558489a3ae6e23f744169c621e9d4
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-11-28 20:09:56 +0100

    ccc

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,12 +7,12 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
-    
-    else{
-        free(tree->left);
-        free(tree->right);
-        free(tree);
-    }
+	if (tree == NULL)
+		return;
+
+	else{
+		free(tree->left);
+		free(tree->right);
+		free(tree);
+	}
 }
