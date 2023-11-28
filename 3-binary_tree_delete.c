6811019bbb1af387e4beac9791acf84e41fa0e29
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-11-28 10:59:50 -0800

    task 3 updates

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,12 +7,9 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
-    
-    else{
-        free(tree->left);
-        free(tree->right);
-        free(tree);
-    }
+	if (tree == NULL)
+		retun;
+	free(tree->left);
+	free(tree->right);
+	free(tree);
 }
