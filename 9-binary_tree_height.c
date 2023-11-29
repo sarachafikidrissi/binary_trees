4537971f0048d2669e451992eec3b408b5ebe2fc
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-11-29 13:07:51 +0100

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,9 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
+
 	if (!tree)
 		return (0);
 
