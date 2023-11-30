d8d0ee36d561ef395864d185b6c08d86e969270d
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-11-30 15:40:29 +0100

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * A fucntion that finds sibling of a node
+ * binary_tree_sibling- A fucntion that finds sibling of a node
  * @node: a pointer to the node to find the sibling
  * Return: A pointer to sibling node , or NULL
 */
